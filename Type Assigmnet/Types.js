e1106e1e6bed419f1ab3c4ff118728bc04c0f828
Future Mulenga <mulengafuture14@gmail.com>
2024-10-17 11:58:25 -1000

    type assigment

---
+++ Type Assigmnet/Types.js
@@ -0,0 +1,6 @@
+//Assigning the type explicitly 
+var FirstName = "Future";
+console.log(FirstName);
+//Assigning the the type implicitly 
+var lastName = 'Mulenga';
+console.log(lastName);
---
+++ Type Assigmnet/Types.ts
@@ -0,0 +1,10 @@
+
+//Assigning the type explicitly 
+var FirstName: string  = "Future";
+
+console.log(FirstName);
+
+
+//Assigning the the type implicitly 
+var lastName = 'Mulenga';
+console.log(lastName);
